95f36fa6638f26c2c38db581aa82168d4b63ad4d
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-04-12 21:51:18 +0100

    [master] Optimize imports.

--- src/main/java/com/spgbt/controller/EmployeeController.java
+++ src/main/java/com/spgbt/controller/EmployeeController.java
@@ -3,7 +3,10 @@ package com.spgbt.controller;
 import com.spgbt.model.Employee;
 import com.spgbt.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employee")
